766a095422effb0ee2d31bf4789147f0da1f65bf
Iftikhar Hossain <iftikhar.tutul@gmail.com>
2022-03-10 23:45:19 +0600

    Update nfs_stale_mount.sh

--- nfs_stale_mount.sh
+++ nfs_stale_mount.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-
-while read _ _ mount _; do 
- read -t1 < <(stat -t "$mount") || echo "$mount"; 
-done < <(mount | grep 'nfs') >> nfs_array.txt
-sleep 5
-
-for i in $(cat nfs_array.txt); do umount -lf $i ; sleep 10; done
+while :
+do
+  while read _ _ mount _; do 
+    read -t1 < <(stat -t "$mount") || echo "$mount"; 
+  done < <(mount | grep 'nfs') >> nfs_array.txt
+  
+  for i in $(cat nfs_array.txt); do umount -lf $i; done
+  sleep 5
+done
